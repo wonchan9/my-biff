7538846013bb3802abd1789acf0b7a3b08f6acef
wonchan9 <imwcjang@gmail.com>
2025-09-03 00:06:49 +0900

    update

--- app/page.js
+++ app/page.js
@@ -21,7 +21,7 @@ export default function Home() {
             .
           </li>
           <li className="tracking-[-.01em]">
-            Save and see your changes instantly. 123123오 신기해
+            Save and see your changes instantly. 123123오 신기해!!
           </li>
         </ol>
 
